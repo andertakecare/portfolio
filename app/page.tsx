9b252d7cadfdd36dc06f47322781dbc991d6c68a
andertakecare <20363451+andertakecare@users.noreply.github.com>
2024-11-01 19:49:05 +0900

    add prettier config to package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 export default function HomePage() {
-    return (
-      <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-800 pt-20">
-          <h2 className="text-4xl font-bold mb-4 text-blue-400">
-            It's me, Ander
-          </h2>
-          <p className="text-lg text-center text-zinc-200">
-            I am a software developer, learning TypeScript, React, Next.js, and Tailwind.
-          </p>
-          <button className="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors">
-            omg
-          </button>
-        <section id="about" className="min-h-screen text-white">about</section>
-        <section id="projects" className="min-h-screen text-white">projects</section>
-        <section id="contact" className="min-h-screen text-white">contact me</section>
-      </main>
-    );
-  }
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-800 pt-20">
+      <h2 className="text-4xl font-bold mb-4 text-blue-400">
+        It&apos;s me, Ander {/*escaped single quote for react*/}
+      </h2>
+      <p className="text-lg text-center text-zinc-200">
+        I am a software developer, learning TypeScript, React, Next.js, and
+        Tailwind.
+      </p>
+      <button className="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors">
+        omg
+      </button>
+      <section id="about" className="min-h-screen text-white">
+        about
+      </section>
+      <section id="projects" className="min-h-screen text-white">
+        projects
+      </section>
+      <section id="contact" className="min-h-screen text-white">
+        contact me
+      </section>
+    </main>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,30 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-export default function Header () {
-    return (
-      <header className="bg-gray-900 text-white w-full py-4 px-8 fixed top-0 z-50">
-        <nav className="flex justify-between items-center">
-          <div className="text-4xl font-bold">
-            <Link href="#home">My Portfolio</Link>
-          </div>
-          <ul className="flex space-x-8 text-2xl">
-            <li>
-              <Link href="#about" scroll={false}>
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="#projects" scroll={false}>
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link href="#contact" scroll={false}>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
-  
+export default function Header() {
+  return (
+    <header className="bg-gray-900 text-white w-full py-4 px-8 fixed top-0 z-50">
+      <nav className="flex justify-between items-center">
+        <div className="text-4xl font-bold">
+          <Link href="#home">My Portfolio</Link>
+        </div>
+        <ul className="flex space-x-8 text-2xl">
+          <li>
+            <Link href="#about" scroll={false}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="#projects" scroll={false}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link href="#contact" scroll={false}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
