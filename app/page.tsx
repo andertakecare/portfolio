2ae14aad15df151b14636b76d9ffe484290d6cab
andertakecare <20363451+andertakecare@users.noreply.github.com>
2024-11-05 07:34:05 +0900

    adjust padding of main sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,16 @@ import ander from '../public/Ander.jpg';
 
 export default function HomePage() {
 	return (
-		<main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-800 pt-20">
-
+		<main className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
 			<section
 				id="about"
-				className="flex flex-col items-center justify-center w-full text-white bg-yellow-700"
+				className="flex flex-col items-center justify-center w-full text-white bg-yellow-700 pt-[5vw]"
 			>
 				<h2 className="text-4xl font-bold mb-4 text-blue-400">
 					Hello, I&apos;m Ander {/*escaped single quote for react*/}
 				</h2>
 				<Image
-					className="xl:w-full lg:w-2/3 md:w-1/2 sm:w-1/4"
+					className="xl:h-1/2 xl:w-1/3 lg:w-2/3 md:w-1/2 sm:w-1/4 "
 					src={ander}
 					alt="Picture of the author"
 				></Image>
@@ -23,10 +22,10 @@ export default function HomePage() {
 					Tailwind.
 				</p>
 			</section>
-			<section id="projects" className="min-h-screen text-white bg-green-700">
+			<section id="projects" className="min-h-screen text-white bg-green-700 pt-[5vw]">
 				projects
 			</section>
-			<section id="contact" className="min-h-screen text-white bg-cyan-700">
+			<section id="contact" className="min-h-screen text-white bg-cyan-700 pt-[5vw]">
 				contact me
 			</section>
 		</main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-gray-900 text-white w-full py-4 px-8 fixed top-0 z-50">
+    <header className="bg-gray-900 text-white w-full h-[5vh] py-4 px-8 fixed top-0 z-50">
       <nav className="flex justify-between items-center">
         <div className="text-4xl font-bold">
           <Link href="#home">My Portfolio</Link>
