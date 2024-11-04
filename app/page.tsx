7cf9a19ad3aa21bb5f48e11c1ec6c0fec89e186d
andertakecare <20363451+andertakecare@users.noreply.github.com>
2024-11-05 08:00:57 +0900

    fixed scroll position from nav Link elements

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,35 @@ import ander from '../public/Ander.jpg';
 
 export default function HomePage() {
 	return (
-		<main className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
+		<main className="flex flex-col items-center justify-center pt-[3vw] min-h-screen bg-gray-800">
 			<section
 				id="about"
-				className="flex flex-col items-center justify-center w-full text-white bg-yellow-700 pt-[5vw]"
+				className="scroll-mt-[3vw] flex flex-col items-center justify-center w-full text-white bg-yellow-900 "
 			>
-				<h2 className="text-4xl font-bold mb-4 text-blue-400">
+				<h2 className="text-4xl font-bold mb-4 text-blue-400 self-center">
 					Hello, I&apos;m Ander {/*escaped single quote for react*/}
 				</h2>
 				<Image
-					className="xl:h-1/2 xl:w-1/3 lg:w-2/3 md:w-1/2 sm:w-1/4 "
+					className="xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-1/2"
 					src={ander}
 					alt="Picture of the author"
 				></Image>
 
-				<p className="text-2xl text-center text-zinc-200">
+				<p className="text-2xl text-center text-zinc-200 py-8 italic">
 					I am a software developer, learning TypeScript, React, Next.js, and
 					Tailwind.
 				</p>
 			</section>
-			<section id="projects" className="min-h-screen text-white bg-green-700 pt-[5vw]">
-				projects
+			<section
+				id="projects"
+				className="scroll-mt-[3vw] min-h-screen w-full text-white bg-green-700"
+			>
+				<h1>Here are some of my projects:</h1>
 			</section>
-			<section id="contact" className="min-h-screen text-white bg-cyan-700 pt-[5vw]">
+			<section
+				id="contact"
+				className="scroll-mt-[3vw] min-h-screen w-full text-white bg-cyan-700"
+			>
 				contact me
 			</section>
 		</main>
