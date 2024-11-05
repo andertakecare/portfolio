0686d0b40068318292239de97c1282f92419e538
andertakecare <20363451+andertakecare@users.noreply.github.com>
2024-11-06 08:00:36 +0900

    resize for very small screens

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import ander from '../public/Ander.jpg';
 
 export default function HomePage() {
 	return (
-		<main className="mt-20 flex flex-col items-center justify-center pt-[3vw] min-h-screen bg-gray-800 z-100">
+		<main className="mt-14 sm:mt-20 flex flex-col items-center justify-center min-h-screen bg-gray-800 z-100">
 			<section
 				id="about"
-				className="flex flex-col items-center justify-center w-full text-white bg-yellow-900 "
+				className="scroll-mt-14 sm:scroll-mt-20 flex flex-col items-center justify-center w-full text-white bg-yellow-900 "
 			>
 				<h2 className="text-4xl font-bold mb-4 text-blue-400 self-center">
 					Hello, I&apos;m Ander {/*escaped single quote for react*/}
@@ -24,13 +24,13 @@ export default function HomePage() {
 			</section>
 			<section
 				id="projects"
-				className="scroll-mt-[3vw] min-h-screen w-full text-white bg-green-700"
+				className="scroll-mt-14 sm:scroll-mt-20 min-h-screen w-full text-white bg-green-700"
 			>
 				<h1>Here are some of my projects:</h1>
 			</section>
 			<section
 				id="contact"
-				className="scroll-mt-[3vw] min-h-screen w-full text-white bg-cyan-700"
+				className="scroll-mt-14 sm:scroll-mt-20 min-h-screen w-full text-white bg-cyan-700"
 			>
 				contact me
 			</section>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,26 @@
 import Link from 'next/link';
 
 export default function Header() {
-  return (
-    <header className="flex items-center bg-gray-900 text-white w-full min-h-fit h-20 py-4 px-8 fixed top-0 z-50">
-      <nav className="flex justify-between w-full">
-        <div className="text-4xl font-bold">
-          <Link href="#home">My Portfolio</Link>
-        </div> 
-        <ul className="flex space-x-8 text-2xl">
-          <li>
-            <Link href="#about">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="#projects">
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link href="#contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="flex items-center bg-gray-900 text-white w-full 
+    h-14 sm:h-14 2xl:h-20
+    py-4 px-8 fixed top-0 z-50">
+			<nav className="flex justify-between w-full">
+				<div className="text-2xl 2xl:text-4xl font-bold">
+					<Link href="#home">My Portfolio</Link>
+				</div>
+				<ul className="flex items-center space-x-8 text-xl 2xl:text-2xl">
+					<li>
+						<Link href="#about">About</Link>
+					</li>
+					<li>
+						<Link href="#projects">Projects</Link>
+					</li>
+					<li>
+						<Link href="#contact">Contact</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
