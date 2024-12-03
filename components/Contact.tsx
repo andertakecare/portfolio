9f8f6a5919246b60fc94c275a6241e0ad20b8f0b
andertakecare <20363451+andertakecare@users.noreply.github.com>
2024-12-03 22:15:46 +0900

    change layout of contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,15 +5,15 @@ export default function Contact() {
 	return (
 		<section
 			id="contact"
-			className="flex scroll-mt-14 sm:scroll-mt-16 2xl:scroll-mt-20 min-h-screen w-full text-white bg-cyan-700"
+			className="flex flex-col scroll-mt-14 sm:scroll-mt-16 2xl:scroll-mt-20 min-h-screen w-full text-white bg-cyan-700"
 		>
-				<div className='w-1/12 flex flex-col justify-center items-center mx-auto'>
+				<div className='w-1/12 flex flex-col justify-center items-center m-auto'>
           <Image
             src={paperAirplaine}
             alt="icon of email"
           ></Image>
-          <h2>Send me an email!</h2>
         </div>
+          <h2 className="text-4xl m-auto">Send me an email!</h2>
 		</section>
 	);
 }
