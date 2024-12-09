d05c721d87e4743f9336d90cebe1a795c1c30dea
andertakecare <20363451+andertakecare@users.noreply.github.com>
2024-12-10 08:06:49 +0900

    add email hyperlink and format contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,19 +1,20 @@
 import Image from 'next/image';
-import paperAirplaine from '../public/paper-airplane.png';
+import paperAirplane from '../public/paper-airplane.png';
 
 export default function Contact() {
 	return (
 		<section
 			id="contact"
-			className="flex flex-col scroll-mt-14 sm:scroll-mt-16 2xl:scroll-mt-20 min-h-screen w-full text-white bg-cyan-700"
+			className="flex flex-col scroll-mt-14 sm:scroll-mt-16 2xl:scroll-mt-20 min-h-[50vh] w-full text-white"
 		>
-				<div className='w-1/12 flex flex-col justify-center items-center m-auto'>
-          <Image
-            src={paperAirplaine}
-            alt="icon of email"
-          ></Image>
-        </div>
-          <h2 className="text-4xl m-auto">Send me an email!</h2>
+			<figure className="flex flex-col items-center w-1/12 m-auto text-center">
+				<a href="mailto:andertakecare@gmail.com">
+					<Image src={paperAirplane} alt="icon of email" />
+				</a>
+				<figcaption className="whitespace-nowrap text-4xl text-white mt-8">
+					Send me an email!
+				</figcaption>
+			</figure>
 		</section>
 	);
 }
